9ef3c0010fa3c1f1e769ab82743ebe44cfe1a927
Dragan Savic <savicdragan2707@gmail.com>
2019-10-13 15:20:26 +0200

    Change work time

--- resources/js/src/modules/front/customer-form/config.js
+++ resources/js/src/modules/front/customer-form/config.js
@@ -1,5 +1,5 @@
 export const START_TIME = '08:00'
-export const WORK_DAY_END_TIME = '22:00'
+export const WORK_DAY_END_TIME = '20:00'
 export const SATURDAY_END_TIME = '13:00'
 
 export const SATURDAY = 6
